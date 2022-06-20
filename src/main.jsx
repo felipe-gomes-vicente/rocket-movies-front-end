1765c20c5fbbeb615d78e58d63d6e09041349884
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-06-20 16:26:11 -0300

    created page Profile

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from "styled-components";
 
-import { SignUp } from "./pages/SignUp";
+import { Profile } from "./pages/Profile";
 
 import theme from "./styles/theme";
 import GlobalStyles from "./styles/global";
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 )
