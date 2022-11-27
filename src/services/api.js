f86b95bee6cbff16b1a03ecb8d358810cf47e47d
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-11-26 21:03:09 -0300

    fix integration with api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'https://rocket-movies.herokuapp.com'
+  // baseURL: 'https://rocket-movies.herokuapp.com'
+  baseURL: 'https://rocket-movies.onrender.com'
 });
